2030d01d7ed18ccac228787630cda7ed30440a9e
Randolf Jung <randolf@mathematic.io>
2025-10-27 08:44:16 +0100

    Use debian images

---
+++ build.dev.ts
@@ -0,0 +1,13 @@
+import { defaultBuildLogger, Template } from "e2b";
+import { template } from "./template";
+
+async function main() {
+	await Template.build(template, {
+		alias: "claude-tools-dev",
+    cpuCount: 2,
+    memoryMB: 2048,
+		onBuildLogs: defaultBuildLogger(),
+	});
+}
+
+main().catch(console.error);
